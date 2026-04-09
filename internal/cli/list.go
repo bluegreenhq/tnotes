0db34f77253db2a6fe0fd543526975b6c70a28bc
Yuichi Ikeda <yuichi.ikeda@blueg.co.jp>
2026-04-09 17:19:20 +0900

    fix list layout

--- internal/cli/list.go
+++ internal/cli/list.go
@@ -3,16 +3,23 @@ package cli
 import (
 	"fmt"
 	"io"
-	"text/tabwriter"
+	"os"
+	"strings"
 
-	"github.com/cockroachdb/errors"
+	"github.com/charmbracelet/x/term"
+	"github.com/mattn/go-runewidth"
 
 	"github.com/bluegreenhq/tnotes/internal/app"
 )
 
 const (
-	tabPadding       = 2
 	minArgsForListFlag = 3
+	colGap             = 2
+	idWidth            = 16
+	updatedWidth       = 16 // "2006-01-02 15:04"
+	fixedColumnsWidth  = idWidth + updatedWidth + colGap*2
+	defaultTermWidth   = 80
+	minTitleWidth      = 10
 )
 
 func runList(args []string, a *app.App, w io.Writer) error {
@@ -38,17 +45,30 @@ func runList(args []string, a *app.App, w io.Writer) error {
 		return nil
 	}
 
-	tw := tabwriter.NewWriter(w, 0, 0, tabPadding, ' ', 0)
-	_, _ = fmt.Fprintln(tw, "ID\tTITLE\tUPDATED")
+	tw := titleWidth()
+	printRow(w, "ID", "TITLE", "UPDATED", tw)
 
 	for _, n := range notes {
-		_, _ = fmt.Fprintf(tw, "%s\t%s\t%s\n", n.ID, n.Title(), n.UpdatedAt.Format("2006-01-02 15:04"))
-	}
-
-	err := tw.Flush()
-	if err != nil {
-		return errors.WithStack(err)
+		title := runewidth.Truncate(n.Title(), tw, "...")
+		updated := n.UpdatedAt.Format("2006-01-02 15:04")
+		printRow(w, string(n.ID), title, updated, tw)
 	}
 
 	return nil
+}
+
+func titleWidth() int {
+	w, _, err := term.GetSize(os.Stdout.Fd())
+	if err != nil || w <= 0 {
+		w = defaultTermWidth
+	}
+
+	return max(w-fixedColumnsWidth, minTitleWidth)
+}
+
+func printRow(w io.Writer, id, title, updated string, tw int) {
+	paddedID := runewidth.FillRight(id, idWidth)
+	paddedTitle := runewidth.FillRight(title, tw)
+	gap := strings.Repeat(" ", colGap)
+	_, _ = fmt.Fprintf(w, "%s%s%s%s%s\n", paddedID, gap, paddedTitle, gap, updated)
 }
